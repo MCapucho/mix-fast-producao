e2bac0e9e5f97182eafd13fa1eab448ad2048cf1
Murylo Capucho <mcapuchoribeiro@gmail.com>
2024-03-13 23:01:18 -0300

    Implementação do projeto

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -16,11 +16,11 @@ variable "name" {
 }
 
 variable "vpc_id" {
-  default = "vpc-0a928f18f52e3f526"
+  default = "vpc-037ba6b40e5c0940e"
 }
 variable "subnet_ids" {
   type = list
-  default = ["subnet-0f1c1df7d6204cd14", "subnet-02360cad76c9230ab", "subnet-055459a695e1caddb"]
+  default = ["subnet-06f7a7892cd3eff1e", "subnet-00cba77ae10a02240", "subnet-0b2aecbce1649de68"]
 }
 variable "network_mode" {
   type = string
@@ -36,7 +36,7 @@ variable "memory" {
 }
 variable "target_group_arn" {
   type = string
-  default = "arn:aws:elasticloadbalancing:us-east-1:022874923015:targetgroup/mixfastproducao-target-group/5753f0cb7bb9193f"
+  default = "arn:aws:elasticloadbalancing:us-east-1:211125470560:targetgroup/mixfastproducao-target-group/95f589f8a9bb7b87"
 }
 variable "port" {
   type = number
